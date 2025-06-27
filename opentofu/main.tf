40ef0a221711cecc3d560056a198a42046ff3d81
eb075324 <eric.botcher@broadcom.com>
2025-06-26 22:33:14 -0700

    bucnh of updates but Ansible is sorta working now

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -11,6 +11,11 @@ resource "aws_instance" "dz1" {
   }
 }
 
+resource "aws_eip" "dz1_ip" {
+  instance = aws_instance.dz1.id
+  vpc      = true
+}
+
 output "dz1_public_ip" {
-  value = aws_instance.dz1.public_ip
+  value = aws_eip.dz1_ip.public_ip
 }
